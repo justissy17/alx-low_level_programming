19ba6f92cbe5d667eac82ca8257869b66128601c
justissy17 <isrealuka@gmail.com>
2023-06-18 19:17:18 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout, 
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout));
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
