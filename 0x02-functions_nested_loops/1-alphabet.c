74e458ac51750a2944c91d5c9ced2f5337405a98
justissy17 <isrealuka@gmail.com>
2023-06-23 11:35:22 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point (print alphabet)
+ * Return: 0(successful)
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+				}
+				_putchar('\n');
+				}
