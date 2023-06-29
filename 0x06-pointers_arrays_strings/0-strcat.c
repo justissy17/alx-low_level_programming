0be2b69a3ff326044cb2f4e45f0abab36c738328
justissy17 <isrealuka@gmail.com>
2023-06-29 16:51:43 -0700

    concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,18 +10,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int dlen =0, i;
+	int i;
+	int j;
 
-	while (dest[dlen])
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dlen++;
+		i++;
 	}
 
-	for (i = 0; src[i] != 0; i++)
+	j = 0;
+	while (src[i] != '\0')
 	{
-		dest[dlen] = src[i];
-		dlen++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	dest[dlen] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
