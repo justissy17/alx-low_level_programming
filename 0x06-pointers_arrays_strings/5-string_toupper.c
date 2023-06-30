2a4f2da9e776ad58bba67715068b346552387cdf
justissy17 <isrealuka@gmail.com>
2023-06-29 18:38:27 -0700

    correction

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
@@ -15,7 +14,7 @@ char *string_toupper(char *n)
 	while (n[i] != '\0')
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
-			n[i] = n[i] - 32;
+		n[i] = n[i] - 32;
 			i++;
 	}
 	return (n);
